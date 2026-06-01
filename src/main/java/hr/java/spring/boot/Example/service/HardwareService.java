1e8e5924c87328726808157eb3a183f12cef3644
Gongamesh <kovacevicstjepan02@gmail.com>
2026-06-01 20:14:28 +0200

    Created HardwareService

---
+++ src/main/java/hr/java/spring/boot/Example/service/HardwareService.java
@@ -0,0 +1,10 @@
+package hr.java.spring.boot.Example.service;
+
+import hr.java.spring.boot.Example.dto.HardwareDTO;
+
+import java.util.List;
+
+public interface HardwareService {
+    List<HardwareDTO> getAllHardware();
+    List<HardwareDTO> getHardwareByCode(String hardwareCode);
+}
